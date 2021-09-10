48fbc5d79ebaa3cac98b0c37c4b142bec49c64da
Theorhetically <56592882+Theorhetically@users.noreply.github.com>
2021-09-10 15:30:40 +0800

    UI

---
+++ DigitalClock.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DigitalClock : MonoBehaviour
+{
+    private Text textClock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        textClock = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour = LeadingZero(time.Hour);
+        string minute = LeadingZero(time.Minute);
+        string second = LeadingZero(time.Second);
+        textClock.text = hour + ":" + minute + ":" + second;
+    }
+
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
